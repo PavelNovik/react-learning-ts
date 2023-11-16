c64985e8f46a3e8670f7a60e71ad3c038b3cef7b
Pavel Novik <brn2befree@gmail.com>
2023-11-16 22:50:46 +0300

    Nothing to change, only update

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,14 +1,14 @@
-import React, {ChangeEvent, useState} from 'react';
-import {CountryType} from "../../App";
+import React, { ChangeEvent, useState } from 'react';
+import { CountryType } from "../../App";
 
 
 const Select = () => {
     const [select, setSelect] = useState<undefined | string>(undefined)
 
-    const countries: CountryType[] = [{title: 'Belarus', id: '1'}, {title: 'Poland', id: '2'}, {
+    const countries: CountryType[] = [{ title: 'Belarus', id: '1' }, { title: 'Poland', id: '2' }, {
         title: 'Ukraine',
         id: '3'
-    }, {title: 'Lithuania', id: '4'}, {title: 'Latvia', id: '5'}]
+    }, { title: 'Lithuania', id: '4' }, { title: 'Latvia', id: '5' }]
     const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
         setSelect(e.currentTarget.value)
     }
