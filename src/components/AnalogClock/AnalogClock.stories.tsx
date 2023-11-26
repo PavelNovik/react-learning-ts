839e6379dc17811e2bbb8484ac106d01949d5bc1
Pavel Novik <brn2befree@gmail.com>
2023-11-26 16:46:45 +0300

    add animation to analog clock component

--- src/components/AnalogClock/AnalogClock.stories.tsx
+++ src/components/AnalogClock/AnalogClock.stories.tsx
@@ -2,10 +2,18 @@ import {AnalogClock} from "./AnalogClock";
 import React, {useEffect, useState} from "react";
 
 export default {
-    title: 'React/Clock Digital-Analog',
+    title: 'React/React.UseEffect/Clock',
 }
 
 export const AnalogClockStory = ()=> {
+    return <AnalogClock isAnalogClock={true} changeClockView={()=>{}}/>
+}
+
+export const DigitalClockStory = ()=> {
+    return <AnalogClock isAnalogClock={false} changeClockView={()=>{}}/>
+}
+
+export const AnalogDigitalClockStory = ()=> {
     const [isAnalogClock, setIsAnalogClock] = useState(true)
     return <AnalogClock isAnalogClock={isAnalogClock} changeClockView={()=>setIsAnalogClock(!isAnalogClock)}/>
 }
